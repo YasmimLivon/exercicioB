9dd04bfe8cf86ee0978fa1d3001a88daa1508f0e
YasmimLivon <yasmim.livon@aluno.senai.br>
2025-06-24 15:08:27 -0300

    Inicializando exercicio B

---
+++ script.js
@@ -0,0 +1,5 @@
+const calendar = document.getElementById("calendar")
+const data1 = document.getElementById("data1")
+const data2 = document.getElementById("data2")
+const enviar = document.getElementById("enviar")
+const itens = document.querySelectorAll("td")
